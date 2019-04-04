43e8b6380af43a6e30b8e6feef26bcba46658194
John Dunne <G00273895@gmit.ie>
2019-04-04 06:57:02 +0100

    Mean petal width

--- Mean_Fisher_Iris.py
+++ Mean_Fisher_Iris.py
@@ -14,11 +14,14 @@ data = np.genfromtxt('Fishers_Iris_data_set.csv',delimiter= ',', skip_header=1)
 # I read about using skip_header here: https://docs.scipy.org/doc/numpy-1.13.0/user/basics.io.genfromtxt.html
 
 meansepal_lenght= np.mean(data[:,0])
-# numpy.mean used to calculate the mean of the first column of data
+# numpy.mean used to calculate the mean of the first column of data and subsequently the remaining columns of data
 meansepal_width= np.mean(data[:,1])
 meanpetal_lenght= np.mean(data[:,2])
+meanpetal_width= np.mean(data[:,3])
 print(f"The mean of the first column of data- sepal lenght is: {meansepal_lenght} rounded to one decimal place {(round(meansepal_lenght, 1))}")
 # The output has been set to return the mean of the first column of data and the mean rounded to one decimal place
 # formatted string used to return the sentence when the program is run
 print(f"The mean of the second column of data- sepal width is: {meansepal_width} rounded to one decimal place {(round(meansepal_width, 1))}")
 print(f"The mean of the third column of data- petal lenght is: {meanpetal_lenght} rounded to one decimal place {(round(meanpetal_lenght, 1))}")
+print(f"The mean of the fourth column of data- petal width is: {meanpetal_width} rounded to one decimal place {(round(meanpetal_width, 1))}")
+print("The fifth column of the data set contains the name of the variation of Iris flower")

516b28a04ceab33d1a11d07e8906f7f9936c2a44
John Dunne <G00273895@gmit.ie>
2019-04-14 12:15:39 +0100

    Dataset_Describe.py - dataset.describe()

--- Dataset_Describe.py
+++ Dataset_Describe.py
@@ -12,3 +12,9 @@ data = "https://raw.githubusercontent.com/johndunne2019/pands-project/master/Fis
 dataset = pd.read_csv(data, header=0)
 # pandas.read used to read in the csv file, I read about it here: https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
 # header=0 used to set the first row of data as the header, read here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+print(dataset.describe())
+# A description of the data set will be printed to the screen 
+# The description will be made up of: count of rows, mean, standard deviation, min and max
+# Also included is lower, middle and upper percentiles which by default are 25%, 50% and 75%
+# I read about what is included in the dataset.describe() output here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
+# I will provide a description of what each line in the output is in my readme file

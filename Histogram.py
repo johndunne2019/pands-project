d8086033ec9e404f80ac0ca0cc96eda91a879540
John Dunne <G00273895@gmit.ie>
2019-04-15 18:37:31 +0100

    Histogram.py

---
+++ Histogram.py
@@ -0,0 +1,10 @@
+# 2019-04-25
+# John Dunne
+# To create a histogram of the data using matplotlib pyplot 
+# Example read here: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# pandas documentation: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.hist.html
+
+import matplotlib.pyplot as pl
+# imported matplotlib.pyplot which will be used to plot the histogram
+import pandas 
+# pandas imported - will be used to read in the data set from my repository

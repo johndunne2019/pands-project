60fbf783beba100f54f943fe6c59dbc226a68911
John Dunne <G00273895@gmit.ie>
2019-04-15 20:28:49 +0100

    Intro_Fisher_Iris.py - Dataframe.tail()

--- Intro_Fisher_Iris.py
+++ Intro_Fisher_Iris.py
@@ -1,5 +1,7 @@
 # 2019-04-13
 # Adapted from: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# Note- script must be run while command prompt/cmder is in full screen mode to view all data
+
 import pandas #imported pandas module
 data = "https://raw.githubusercontent.com/johndunne2019/pands-project/master/Fishers_Iris_data_set.csv"
 # url given to data set saved in my repository. The data is read in a as a data frame. 
@@ -9,8 +11,12 @@ dataset = pandas.read_csv(data, header=0)
 # pandas.read used to read the csv file - https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
 # header=0 added as without this the first row was being printed to the screen twice
 # read about header=0 here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
-print("Let's have a peek at the data set. The first 10 rows of the data set are shown below.")
+print("Let's have a peek at the data set. The first 10 rows of the data set are shown below:")
 print(dataset.head(10))
-# dataset.head prints rows to the screen from the parameter entered. In this case I have entered 10 to print the first 10 rows.
-# Note- script must be run while command prompt/cmder is in full screen mode to view all data
+# dataframe.head() prints rows to the screen from the parameter entered. In this case I have entered 10 to print the first 10 rows.
+
+print("The last ten rows of the data set are shown below:")
 print(dataset.tail(10))
+# dataframe.tail() used to print the last ten rows of data to the screen
+# I have entered the parameter of 10, the default parameter is 5 
+# Read about dataframe.tail() here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.tail.html and http://www.datasciencemadesimple.com/head-and-tail-in-python-pandas/

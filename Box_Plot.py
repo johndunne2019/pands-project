ea3eaaf46cb04a9fa85a923fe34cd649aa95041a
John Dunne <G00273895@gmit.ie>
2019-04-24 21:10:21 +0100

    Box_Plot.py

--- Box_Plot.py
+++ Box_Plot.py
@@ -1,7 +1,8 @@
 # 2019-04-24
 # John Dunne
-# Scatter plot of the data set
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.scatter.html
+# Box plot of the data set
+# pandas box plot documentation: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.boxplot.html
+print("The box plot will appear on the screen momentarily")
 import matplotlib.pyplot as pl
 import pandas as pd
 # imported the libraries needed and give shortened names
@@ -9,6 +10,9 @@ data = "https://raw.githubusercontent.com/johndunne2019/pands-project/master/Fis
 # url of the data set to be read by pandas.read
 dataset = pd.read_csv(data, header=0)
 # pandas.read used to read in the data set from my repository, header set to first row of data in the data set
-dataset.boxplot(by= 'species')
+dataset.boxplot(by= 'species', grid=True)
+# pandas dataframe.boxplot used to plot box plot of data set: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.boxplot.html
+# Added grid and set box plot to group by species
 # http://cmdlinetips.com/2018/03/how-to-make-boxplots-in-python-with-pandas-and-seaborn/
 pl.show()
+# pyplot.show() command shows the histogram on the screen 

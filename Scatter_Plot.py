d7aaf6c09372396a3ce38f4c0bb62139d5efbf7e
John Dunne <G00273895@gmit.ie>
2019-04-24 20:44:04 +0100

    Scatter_Plot.py

---
+++ Scatter_Plot.py
@@ -0,0 +1,9 @@
+# 2019-04-24
+# John Dunne
+# Scatter plot of the data set
+
+import matplotlib.pyplot as pl
+import pandas as pd
+# imported the libraries needed 
+data = "https://raw.githubusercontent.com/johndunne2019/pands-project/master/Fishers_Iris_data_set.csv"
+# url of the data set to be read by pandas.read

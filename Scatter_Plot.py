e05f21de3be6b1aec1443c720ec0e78d2e0512d5
John Dunne <G00273895@gmit.ie>
2019-04-24 20:45:51 +0100

    Scatter_Plot.py

--- Scatter_Plot.py
+++ Scatter_Plot.py
@@ -7,3 +7,5 @@ import pandas as pd
 # imported the libraries needed 
 data = "https://raw.githubusercontent.com/johndunne2019/pands-project/master/Fishers_Iris_data_set.csv"
 # url of the data set to be read by pandas.read
+dataset = pd.read_csv(data, header=0)
+# pandas.read used to read in the data set from my repository, header set to first row of data in the data set
